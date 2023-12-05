6df49bab9812c13ce58b0ada32b5afd4d84b69ca
Zeedah-kuks <kukumazeedah09@gmail.com>
2023-12-04 22:59:31 -0800

    find the power of number

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,5 +1,5 @@
 #include "main.h"
-/** 
+/**
  * _pow_recursion - power of a number
  * @x: number to be raised
  * @y: power
@@ -11,5 +11,5 @@ int _pow_recursion(int x, int y)
 		return (-1);
 	if (y == 0)
 		return (1);
-	return (x * _pow_recursion(x, y -1));
+	return (x * _pow_recursion(x, y - 1));
 }
