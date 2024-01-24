102c49c4e34df81fe5ad01416eaec51b377937e4
Zeedah_kuks <kukumazeedah09@gmail.com>
2024-01-24 06:58:31 -0800

    a program that prints its name, followed by a new line

---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,2 @@
+#0x0A-argc_argv
+This directory contains tasks on argument to command
---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,6 @@
+#define MAIN_H_
+
+int _putchar(char c);
+int main(int argc __attribute__((unused)), char *argv[]);
+
+#endif
